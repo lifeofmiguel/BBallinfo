6d2d1ee607fd941d7c76bfd72fbb749df11dfffb
Miguel Angel Morales <mmorales21500@live.com>
2021-02-26 14:17:56 -0500

    created display method to show full nameof team

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,7 @@
 class CLI
     def run
         greeting
+        display
     end
 
     def greeting
@@ -8,5 +9,10 @@ class CLI
         API.new.get_basketball_data
     end
 
+    def display
+        Bballinfo.all.collect do |team|
+            puts "#{team.full_name}"
+        end
+    end
 
 end
